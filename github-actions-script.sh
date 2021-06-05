b436d589728fa1d54effbbf92ac293bb5336fe9a
vivek-Softwiz <84830806+vivek-Softwiz@users.noreply.github.com>
2021-06-04 22:19:24 -0700

    Create github-actions-script.sh

---
+++ github-actions-script.sh
@@ -0,0 +1,9 @@
+ ls -a
+         echo
+         echo java is installed?
+         java --version 
+         echo 
+         echo is git installed?
+         git --version
+         echo what about build tools?
+         mvn --version
