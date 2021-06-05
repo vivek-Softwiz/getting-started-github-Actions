a3199653cffa38366ad25c0307121a71eb24e090
vivek-Softwiz <84830806+vivek-Softwiz@users.noreply.github.com>
2021-06-04 22:43:07 -0700

    Update github-actions-script.sh

--- github-actions-script.sh
+++ github-actions-script.sh
@@ -1,9 +1,2 @@
          ls -a
-         echo
-         echo java is installed?
-         java --version 
-         echo 
-         echo is git installed?
-         git --version
-         echo what about build tools?
-         mvn --version
+         
